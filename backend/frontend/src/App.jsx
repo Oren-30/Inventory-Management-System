58bcbe6fe11f04ed66b0c98eb9670eca60ea7ae6
sirkiruthi <orencaroli@gmail.com>
2026-07-05 18:41:42 +0300

    Feat add changes to App.jsx

---
+++ backend/frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./components/Navbar";
+import AddProduct from "./components/AddProduct";
+import InventoryList from "./components/InventoryList";
+import ProductSearch from "./components/ProductSearch";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="container">
+
+        <AddProduct />
+
+        <ProductSearch />
+
+        <InventoryList />
+
+      </div>
+    </>
+  );
+}
+
+export default App;
